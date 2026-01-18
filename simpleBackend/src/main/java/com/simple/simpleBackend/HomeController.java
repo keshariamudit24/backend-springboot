29cfd6af52d37cce43cd8bebc05689cb78e676b5
keshariamudit24 <keshariamudit@gmail.com>
2026-01-18 11:58:06 +0530

    basic controller to listen client http requests

---
+++ simpleBackend/src/main/java/com/simple/simpleBackend/HomeController.java
@@ -0,0 +1,14 @@
+package com.simple.simpleBackend;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Welcome to home page";
+    }
+}
---
+++ simpleBackend/src/main/java/com/simple/simpleBackend/SimpleBackendApplication.java
@@ -0,0 +1,15 @@
+package com.simple.simpleBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBackendApplication.class, args);
+	}
+
+}
+
+// ./mvnw spring-boot:run 
