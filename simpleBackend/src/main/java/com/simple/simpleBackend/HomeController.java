57a34549c1871b8b1991e767bc749001beafcb16
keshariamudit24 <keshariamudit@gmail.com>
2026-01-18 12:08:25 +0530

    adding more endpoints (sending data from the server using @ResController and not a page for UI)

--- simpleBackend/src/main/java/com/simple/simpleBackend/HomeController.java
+++ simpleBackend/src/main/java/com/simple/simpleBackend/HomeController.java
@@ -1,9 +1,11 @@
 package com.simple.simpleBackend;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// use @RestController instead of @Controller 
+// or
+// use @Controller with @ResponseBody above the greet method
 @RestController
 public class HomeController {
 
@@ -11,4 +13,10 @@ public class HomeController {
     public String greet(){
         return "Welcome to home page";
     }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "This is the about page";
+    }
+    
 }
