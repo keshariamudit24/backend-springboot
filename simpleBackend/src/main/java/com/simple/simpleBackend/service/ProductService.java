9cdd9b0cf440ec0e3d0e233a438d58c062c8705e
keshariamudit24 <keshariamudit@gmail.com>
2026-03-24 22:57:45 +0530

    service layer fix

--- simpleBackend/src/main/java/com/simple/simpleBackend/service/ProductService.java
+++ simpleBackend/src/main/java/com/simple/simpleBackend/service/ProductService.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import com.simple.simpleBackend.model.Product;
 import com.simple.simpleBackend.repository.ProductRepo;
 
-@RestController
+@Service
 public class ProductService {
 
     @Autowired
